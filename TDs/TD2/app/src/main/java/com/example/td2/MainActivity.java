f662800a8a65945097176aa949fa7e731dbbfe64
Meryeme ATABIT <meryemeatabit1234@gmail.com>
2020-11-10 15:10:16 +0100

    commit project

--- TDs/TD2/app/src/main/java/com/example/td2/MainActivity.java
+++ TDs/TD2/app/src/main/java/com/example/td2/MainActivity.java
@@ -9,6 +9,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.calendar);
+        //setContentView(R.layout.relative_view);
+        //setContentView(R.layout.constraint_view);
+        setContentView(R.layout.repas);
+        //setContentView(R.layout.meteo);
+
+
     }
 }
