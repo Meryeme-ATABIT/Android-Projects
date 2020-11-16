a5d38475ec851fccc4ff4c17de137c50db144c6b
Meryeme ATABIT <meryemeatabit1234@gmail.com>
2020-11-16 20:59:19 +0100

    first commit of TD3

--- TDs/TD3/app/src/main/java/com/example/td3/MainActivity.java
+++ TDs/TD3/app/src/main/java/com/example/td3/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.td3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            // récuperer l'objet button
+        Button rollButton = (Button) findViewById(R.id.lancer);
+        rollButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                EditText edit_text = findViewById(R.id.nombre_face);
+                int nbfaces = Integer.parseInt(String.valueOf(edit_text.getText()));
+
+                // --------- Dé 1 --------------
+                // génerer un nombre au hazard
+                int random1 = new Random().nextInt((nbfaces - 1) + 1) + 1;
+                TextView result1 = (TextView) findViewById(R.id.result1);
+
+                // convertire le nombre entier en string pour l'afficher dans la zone de textView
+                result1.setText(Integer.toString(random1));
+
+            }
+        });
     }
 }
